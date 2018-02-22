15b086b61952f3183a509d67667b2389cce01b9e
Ryan Marks <Ryan@paleo2go@gmail.com>
2018-02-22 19:12:25 +0000

    Comments

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,4 +1,8 @@
 class Gram < ApplicationRecord
+  validates :message, presence: true
+  validates :picture, presence: true 
+  has_many :comments 
+  
   mount_uploader :picture, PictureUploader
   
   validates :message, presence: true
--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
 
+  
+
   describe "grams#destroy action" do
     it "shouldn't allow users who didn't create the gram to destroy it" do
       gram = FactoryBot.create(:gram)
@@ -161,7 +163,13 @@ RSpec.describe GramsController, type: :controller do
       user = FactoryBot.create(:user)
       sign_in user
       
-      post :create, params: { gram: { message: 'Hello!' } }
+      post :create, params: {
+        gram: {
+          message: 'Hello!',
+          picture: fixture_file_upload("/picture.png", 'image/png')
+        }
+      }
+
       expect(response).to redirect_to root_path
 
       gram = Gram.last
